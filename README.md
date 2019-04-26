aa57ee28c27b8404f1d29c77e2bbea0cb295b4a2
AaronJo <mcharima5@gmail.com>
2019-04-26 10:08:30 +0900

    update

--- README.md
+++ README.md
@@ -31,7 +31,9 @@ function callNative(command, args, callback) {
     let query = btoa(encodeURIComponent(JSON.stringify(jsonObject)));
 
     if (window.AndroidBridge) {
-        window.AndroidBridge.callNativeMethod("native://callNative?" + query);
+        AndroidBridge.callNativeMethod("native://callNative?" + query);
+    } else if (/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
+        window.location.href = "native://callNative?" + query;
     } else {
         console.warn("Native calls are not supported.");
     }
--- app/src/main/assets/www/dist/snc/js/nativeBridge.js
+++ app/src/main/assets/www/dist/snc/js/nativeBridge.js
@@ -13,8 +13,8 @@ function callNative(command, args, callback) {
     let query = btoa(encodeURIComponent(JSON.stringify(jsonObject)));
 
     if (window.AndroidBridge) {
-        window.AndroidBridge.callNativeMethod("native://callNative?" + query);
-    } else if (/(iPhone|iPod|iPad).*AppleWebKit/i.test(navigator.userAgent)) {
+        AndroidBridge.callNativeMethod("native://callNative?" + query);
+    } else if (/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
         window.location.href = "native://callNative?" + query;
     } else {
         console.warn("Native calls are not supported.");
